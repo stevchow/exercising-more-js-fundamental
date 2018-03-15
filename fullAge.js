8bb8e9fa6489979f073c16e05ff0060e1412a580
stevchow <stephen_chow97@yahoo.co.id>
2018-03-15 22:53:13 +0700

    fullage

---
+++ fullAge.js
@@ -0,0 +1,23 @@
+/*
+1. Create an array with some years where persons were born
+2. Create an empty array (just [] )
+3. Use a loop to fill the array with the ages of the persons
+4. Use another loop to log into the console whether each person is of full age (18 or older), as well as their age
+
+5. Finally, create a function called printFullAge which receives the array of years as an argument, executes the steps 2., 3. and 4. and returns an array of true/false boolean values: true if the person is of full age (>= 18 years) and false if not (< 18 years)
+6. Call the function with two different arrays and store the results in two variables: full_1 and full_2
+
+Example input:  [1965, 2008, 1992]
+Example output: [true, false, true]
+*/
+
+const years = [1965, 2008, 1992];
+
+function fullAge(arr) {
+  const now = 2018;
+  return arr.map(a => (now - a >= 18 ? true : false));
+}
+
+fullAge(years);
+
+[true, false, true];
